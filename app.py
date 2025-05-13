293d54b4ebba03009fe64d48f05acc0b384d539b
Ishan P <132776204+ScoobyP@users.noreply.github.com>
2025-05-13 15:00:32 +0900

    Update app.py disabled cache

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app_mode = st.sidebar.radio("Select Project",
 
 
 
-@st.cache_resource
+#@st.cache_resource
 def load_sentiment_model():
     pipeline = pickle.load(open('sentiment_pipeline5.pkl', 'rb'))
     return pipeline
