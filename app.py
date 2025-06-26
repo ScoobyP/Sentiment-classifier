8468ab43a448cdcd84ae496dbac04e44959bc911
ishanp <eshanpurekar@gmail.com>
2025-06-26 22:18:00 +0900

    pipeline10

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app_mode = st.sidebar.radio("Select Project",
 
 @st.cache_resource
 def load_sentiment_model():
-    pipeline = joblib.load(open('sentiment_pipeline9.pkl', 'rb'))
+    pipeline = joblib.load('sentiment_pipeline10.pkl')
     return pipeline
 
 # Spam Classifier Page
